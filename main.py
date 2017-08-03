d7e53ffa3e465af838c4bbe57399fd98fd781424
mdruwe <mdruwe@cisco.com>
2017-08-03 19:23:10 +0200

    respond via webhook

--- main.py
+++ main.py
@@ -9,24 +9,6 @@ os.environ['SPARK_ACCESS_TOKEN'] = config.spark_token
 
 api = CiscoSparkAPI() 
 
-# @post('/')
-# def index(request):
-#     """
-#     When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
-#     using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
-#     further actions are taken. i.e.
-#     /batman    - replies to the room with text
-#     /batcave   - echoes the incoming text to the room
-#     /batsignal - replies to the room with an image
-#     """
-#     webhook = json.loads(request.body)
-#     messageId = format(webhook['data']['id'])
-#     roomId = format(webhook['data']['roomId'])
-#     message = api.messages.get(messageId)
-#     text = message.text
-#     print("Message text: " + text)
-#     return "true"
-
 
 def processMessage(message):
     for message in messages:
@@ -43,6 +25,27 @@ def getBotId():
     _me = api.people.me()
     return _me.id
 
+@post('/')
+def index(request):
+    """
+    When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
+    using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
+    further actions are taken. i.e.
+    /batman    - replies to the room with text
+    /batcave   - echoes the incoming text to the room
+    /batsignal - replies to the room with an image
+    """
+    webhook = json.loads(request.body)
+    messageId = format(webhook['data']['id'])
+    roomId = format(webhook['data']['roomId'])
+    message = api.messages.get(messageId)
+    text = message.text
+    print("Message text: " + text)
+    return "true"
+
+
+
+
 
 
 
@@ -51,18 +54,18 @@ def getBotId():
 # bot_name = "Merakify"
 # bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
 botId = getBotId()
-#run_itty(server='wsgiref', host='0.0.0.0', port=3010)
+run_itty(server='wsgiref', host='0.0.0.0', port=3010)
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    while True:
-        rooms = api.rooms.list() #get list of rooms bot is in
-        print(rooms) #generator object
-        for room in rooms:
-            print(room.title) 
-            roomId = room.id
-            messages = api.messages.list(roomId)
-            processMessage(messages)
-        time.sleep(10)
+#     while True:
+#         rooms = api.rooms.list() #get list of rooms bot is in
+#         print(rooms) #generator object
+#         for room in rooms:
+#             print(room.title) 
+#             roomId = room.id
+#             messages = api.messages.list(roomId)
+#             processMessage(messages)
+#         time.sleep(10)
 
